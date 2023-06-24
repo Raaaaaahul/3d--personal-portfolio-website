b0efdfe46c3e447c4910678c1020d9233a5966e4
Rahul <92531518+Raaaaaahul@users.noreply.github.com>
2023-06-24 20:18:34 +0530

    24

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,7 @@ const Hero = () => {
             </p>
         </div>
       </div>
+      
       <ComputersCanvas />
       
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
